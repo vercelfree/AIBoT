662682d61f71bfcd8bc4dd5f59d8744f7f540915
Mr Vercel Boy <saydipc@gmail.com>
2025-08-23 20:08:11 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './Home'
 import BanglaTranslator from './pages/Translate'
 import Correction from './pages/Correction'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/translate" element={<BanglaTranslator />} />
-        <Route path="/correction" element={<Correction />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/translate" element={<BanglaTranslator />} />
+      <Route path="/correction" element={<Correction />} />
+    </Routes>
   )
 }
 
